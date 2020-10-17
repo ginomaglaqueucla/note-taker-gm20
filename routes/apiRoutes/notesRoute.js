bcb0f21a41ca276e52216805b2b9bb2b1e891424
ginomaglaque <ginomaglaque@gmail.com>
2020-10-16 22:18:06 -0500

    test

---
+++ routes/apiRoutes/notesRoute.js
@@ -0,0 +1,9 @@
+const { filterByQuery } = require('../../lib/notes');
+const { db } = require('../../db/db');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+  let results = notes;
+  console.log(results);
+  res.json(results);
+});
